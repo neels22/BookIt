6c66a6e587586b413a6024b8b7a4e9ec7f35f984
neels22 <indraneelsarode@gmail.com>
2025-05-04 21:31:35 -0700

    header added

--- app/layout.js
+++ app/layout.js
@@ -11,7 +11,7 @@ export default function RootLayout({ children }) {
         suppressHydrationWarning
       >
         <AuthProvider>
-          
+          <Header/>
         {children}
         </AuthProvider>
       </body>
